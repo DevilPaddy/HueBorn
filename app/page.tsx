b55b97e5e149ce29dcbd5ce4ea170fb786fbf451
Devilpaddy <belsareanuj7+git@gmail.com>
2025-10-23 13:38:58 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,101 @@
-import Image from "next/image";
+'use client'
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar'
+import { IoIosArrowRoundForward } from "react-icons/io";
+import { FaInstagram } from "react-icons/fa";
+import { FiFacebook } from "react-icons/fi";
+import { SlSocialTwitter } from "react-icons/sl";
+import Link from "next/link";
 
-export default function Home() {
+const page = () => {
+
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <div>
+      {/* nav section */}
+      <Navbar />
+      {/* hero section */}
+      <div className='md:relative h-auto md:h-screen w-full md:px-0 md:py-0 px-[1.3vh] py-[.2vh] bg-[#ede8e2] mb-10 md:mb-0'>
+        <img
+          className='w-full md:h-full h-[46vh] md:z-[-2] object-cover object-right-top md:object-top
+          rounded-[33px] md:rounded-none'
+          src="/hero.jpg" alt="" />
+
+        <div className="over md:absolute md:top-0 md:left-0 w-full 
+          md:h-full z-50 md:bg-[#11111170] 
+          md:px-[3.8vw] md:py-[.2vw] px-0 py-0">
+
+          <div className="hero-content w-full md:w-[60%] h-auto md:h-full flex flex-col  md:justify-center items-stretch md:items-start mt-4 md:mt-0">
+
+            <h6 className='hero-text text-[5vh] font-bold md:text-[8vw] text-[#0f0f10] md:text-[#ede8e2] leading-10 md:leading-[7.4vw]'
+            >Dress for the Life You Desire.
+            </h6>
+
+            <button
+              className='hero-btn md:w-fit mt-[4vh] md:mt-[3.6vw] px-[3vh] py-[1vh] text-[2.6vh] md:px-[1.8vw] md:py-[.6vw] md:text-[1.4vw] font-semibold'
+            >Explore Collections</button>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      </div>
+
+      {/* footer section */}
+      <div className="footer md:h-auto w-full md:px-[3.8vw] md:py-[.2vw] px-[3vh] py-[.2vh]">
+        <div className="abv flex flex-col md:flex-row gap-8 md:justify-evenly mt-10 md:mt-4">
+          <div className='flex flex-col md:w-[16vw] w-auto'>
+            <h6 className='footer-logo'>HueBorn</h6>
+            <p className='text-[#949494] font-semibold'>The art of unhurried living, expressed through curated style.</p>
+          </div>
+
+          <div className='flex flex-col'>
+            <h6>shop</h6>
+            <a className='text-[#949494] font-semibold' href="/">Linen Shirt</a>
+            <a className='text-[#949494] font-semibold' href="/">Polo Shirt</a>
+            <a className='text-[#949494] font-semibold' href="/">Trousers</a>
+            <a className='text-[#949494] font-semibold' href="/">Shoes</a>
+          </div>
+
+          <div className='flex flex-col'>
+            <h6>Stay Connected</h6>
+            <p className='text-[#949494] font-semibold'>Join our journal for insights on style and curation.</p>
+            <div className="email-form mt-4 flex items-center h-[56px]">
+              <input
+                className='px-3 h-full flex-1 bg-[#1e1e1e] rounded-l-sm text-[#949494] outline-none'
+                type="text"
+                placeholder='Your Email' />
+
+              <button
+                className='px-4 h-full bg-[#005f60] rounded-r-sm text-[#949494]'
+              >
+                <IoIosArrowRoundForward size='6vh' />
+              </button>
+            </div>
+          </div>
+
+          <div className='flex flex-col'>
+            <h6>Follow Us</h6>
+            <div className='text-[#949494] flex items-center justify-start md:justify-between gap-3'>
+              <Link href='https://www.instagram.com'>
+                <FaInstagram size='6vh' />
+              </Link>
+              <Link href='https://www.instagram.com'>
+                <FiFacebook size='6vh' />
+              </Link>
+              <Link href='https://www.instagram.com'>
+                <SlSocialTwitter size='6vh' />
+              </Link>
+            </div>
+          </div>
         </div>
-      </main>
+
+        <div className="br h-px bg-zinc-800 mt-8 mb-8"></div>
+        <div className="blo flex flex-col items-center justify-center gap-2.5 mb-10">
+          <p className='text-[#6a7077]'>© 2025 Hueborn. All Rights Reserved.</p>
+          <a href='/' className='text-[#6a7077]'> Privacy Policy · Terms of Service</a>
+        </div>
+
+      </div>
     </div>
-  );
+  )
 }
+
+export default page
---
+++ components/Navbar.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { FaRegHeart } from "react-icons/fa";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const page = () => {
+      const [isHovered, setIsHovered] = useState(false);
+    
+    return (
+        <div className="nav w-full md:px-[3.8vw] md:py-[.2vw] bg-[#ede8e2]
+          flex justify-between items-center px-[3vh] py-[.2vh]">
+            <h1 className='logo text-[6vh] md:text-[2.8vw] font-semibold'>hb</h1>
+            <div className='hidden md:flex nav-menu justify-between items-center gap-[1vw] text-[1.2vw] font-medium'>
+                <a href="">HOME</a>
+                <a href="">CURATION</a>
+                <a href="">ARTIST</a>
+                <a href="">ABOUT US</a>
+            </div>
+
+            <div className='hidden md:block'>
+                <FaRegHeart size="1.8vw"
+                    color={isHovered ? '#005f60' : '#6a7077'}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                />
+            </div>
+
+            <div className="hamburger md:hidden">
+                <RxHamburgerMenu size="4vh"
+                    color={isHovered ? '#005f60' : '#6a7077'}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                />
+            </div>
+        </div>
+    )
+}
+
+export default page
